eb8e982ee4dffcf6ec743c0d95f061cd8f0252b8
brodrickgeorge <brodrick.george@yahoo.com>
2020-12-24 14:11:53 -0600

    second commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Recipe.destroy_all
+Comment.destroy_all
+Rating.destroy_all
+
+
+= User.create(name: "", username: "")
+= User.create(name: "", username: "")
+= User.create(name: "", username: "")
+= User.create(name: "", username: "")
+
+Recipe.create(title: "", content: "", image: "", ingredients: "", vegetarian: )
