983e50adea2da4162a71cb8807add0c4798ed989
brodrickgeorge <brodrick.george@yahoo.com>
2020-12-24 23:51:32 -0600

    finished controllers

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,27 +1,43 @@
 class RatingsController < ApplicationController
     
     def show
-
+        find_rating
+        render json: find_rating, except: [:created_at, :updated_at]
     end
 
     def new 
-
+        rating = Rating.new
     end
 
     def create 
-
+        rating = Rating.new(rating_params)
+        if rating.save
+            render json: rating, except: [:created_at, :updated_at]
+        end
     end
 
     def edit 
-
+        find_rating
     end
 
     def update
-
+        find_rating.update(rating_params)
+        render json: find_rating, except: [:created_at, :updated_at]
     end
 
     def destroy
+        find_rating.destroy
+    end
 
+    private 
+
+    def find_rating
+        rating = Rating.find(params[:id])
+
+    end
+
+    def rating_params
+        params.require(:offer).permit(:rating, :user_id, :recipe_id)
     end
 
 end
