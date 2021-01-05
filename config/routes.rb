c380a703cc343fb55ef96c5ce0b68d84ac5643a3
ctoma <ctomashot@gmail.com>
2021-01-05 15:12:34 -0600

    fixed delete route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :ratings, only: [:index, :show, :create, :update, :delete]
-  resources :comments, only: [:index, :show, :delete, :create]
+  resources :comments
   resources :recipes
   resources :users, only: [:index, :create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
